95dee5bc53411a8142c6f5e7a6262c0c8cdebb4a
Kaspek2480 <matipole@gmail.com>
2022-05-13 12:58:17 +0200

    Fix

--- src/main/java/org/example/StatystykiDruzyny.java
+++ src/main/java/org/example/StatystykiDruzyny.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import org.example.objects.Druzyna;
+
+import java.util.List;
+
 public interface StatystykiDruzyny {
+    public int liczbaDruzyn(List<Druzyna> druzyny);
+
+    public int liczbaDruzynZwyciestw(List<Druzyna> druzyny);
+
+    public int liczbaDruzynRemis(List<Druzyna> druzyny);
+
+    public int liczbaDruzynPorazek(List<Druzyna> druzyny);
 
 }
