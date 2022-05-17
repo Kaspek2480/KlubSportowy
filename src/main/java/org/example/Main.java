04f4ca693af07eb0089bf3de4b1c2072c5fbf960
Michał Skawski <mskawski1025@gmail.com>
2022-05-17 11:04:47 +0200

    pokazalem cycki

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,5 +34,6 @@ public class Main {
     private static void menu() {
         System.out.println("==============================");
         System.out.println("1. Wyświetl ");
+        System.out.println("2.Pokaz cyce ");
     }
 }
